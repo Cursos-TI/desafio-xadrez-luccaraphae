8bbd04883003cb47acddd22c28f6d238db5d9d43
Lucca Raphael <55675031+luccaraphae@users.noreply.github.com>
2025-04-26 02:40:19 +0000

    Implementação da lógica de estrutura de repetições - Nível Novato

--- xadrez.c
+++ xadrez.c
@@ -6,16 +6,83 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int opcao, movimento;
+    int contador = 1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //Exibindo as opções de peças, pedindo ao usuário que escolha e atribuindo o valor à variavél
+    printf("Vamos jogar Xadrez?\n"
+           "Digite o número da peça que deseja movimentar: \n"
+           "1 - Torre\n"
+           "2 - Bispo\n"
+           "3 - Rainha\n");
+    scanf("%d", &opcao);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    
+    //Decidindo a caso de acordo com a opção selecionada pelo usuário
+    switch (opcao)
+    {
+
+        // Implementação de Movimentação da Torre com estrutura DO WHILE
+        case 1:
+
+            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
+            da estrutura de repetição*/
+            printf("Digite a quantidade de casas que a sua Torre deve avançar: ");
+            scanf("%d", &movimento);
+
+            do
+            {
+                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
+                de movimentação da peça*/
+                printf("Direita\n");
+                contador ++;
+            } while (contador <= movimento);
+        break;
+
+
+        // Implementação de Movimentação do Bispo com estrutura WHILE
+        case 2:
+
+            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
+            da estrutura de repetição*/
+            printf("Digite a quantidade de casas que o seu Bispo deve avançar: ");
+            scanf("%d", &movimento);
+
+            while (contador <= movimento)
+            {
+                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
+                de movimentação da peça*/
+                printf("Cima, esquerda\n");
+                contador ++;
+            }
+        break;
+
+        
+        // Implementação de Movimentação da Rainha com estrutura FOR
+        case 3:
+
+            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
+            da estrutura de repetição*/
+            printf("Digite a quantidade de casas que a sua Rainha deve avançar: ");
+            scanf("%d", &movimento);
+
+            for (contador = 1; contador < movimento; contador++)
+            {
+                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
+                de movimentação da peça*/
+                printf("Esquerda\n");
+            };
+        break;
+
+
+        // Caso nenhum número válido para as opções seja digitado
+        default:
+            printf("Opçãos inválida!");
+        break;
+    };
+
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
