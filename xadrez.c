59aa74493c9342da92c57ffe826872938e3c186d
Lucca Raphael <55675031+luccaraphae@users.noreply.github.com>
2025-05-09 01:05:48 +0000

    Nível Mestre  - recursividade e loops complexos

--- xadrez.c
+++ xadrez.c
@@ -1,16 +1,77 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+// Função para validar a opção de direção e retornar em uma variável string
+char *validaDirecao(int opcao)
+{
+
+    char *direcao;
+
+    // Atribuindo uma string de acordo com o número da opção escolhida para o movimento final
+    switch (opcao)
+    {
+    case 1:
+        direcao = "Cima";
+        break;
+    case 2:
+        direcao = "Baixo";
+        break;
+    case 3:
+        direcao = "Esquerda";
+        break;
+    case 4:
+        direcao = "Direita";
+        break;
+    default:
+        printf("Opção inválida");
+        break;
+    };
+
+    return direcao;
+}
+
+// Função para oferecer ao usuário as opções de direção e de acordo com sua resposta atribuir essa opção ao uma variável string
+char *opcoesMovimento()
+{
+
+    char *direcao;
+    int opcao;
+
+    // Pedindo ao usuário a direção do movimento
+    printf("1 - Para cima\n"
+           "2 - Para baixo\n"
+           "3 - Para esquerda\n"
+           "4 - Para direita\n");
+    // Lendo a opção escolhida e armazendo na variável
+    scanf("%d", &opcao);
+
+    direcao = validaDirecao(opcao);
+
+    return direcao;
+}
+
+// Função para a repetitividade do movimento da Rainha
+void recursividade(int repeticao, char *direcao)
+{
+
+    if (repeticao > 0)
+    {
+
+        printf("%s\n", direcao);
+
+        recursividade(repeticao - 1, direcao);
+    };
+}
+
+int main()
+{
     // Nível Novato - Movimentação das Peças
     int opcao, movimento;
     int contador = 1;
+    char *direcao;
 
-
-    //Exibindo as opções de peças, pedindo ao usuário que escolha e atribuindo o valor à variavél
+    // Exibindo as opções de peças, pedindo ao usuário que escolha e atribuindo o valor à variavél
     printf("MateCheck - Vamos jogar Xadrez?\n"
            "Digite o número da peça que deseja movimentar: \n"
            "1 - Torre\n"
@@ -19,158 +80,112 @@ int main() {
            "4 - Cavalo\n");
     scanf("%d", &opcao);
 
-    
-    //Decidindo a caso de acordo com a opção selecionada pelo usuário
+    // Decidindo a caso de acordo com a opção selecionada pelo usuário
     switch (opcao)
     {
+    // Implementação de Movimentação da Torre
+    case 1:
 
-        // Implementação de Movimentação da Torre com estrutura DO WHILE
-        case 1:
+        // Solicitando e atribuindo a string com a direção de movimento da peça
+        printf("\nDigite a direção do movimento da sua Torre:\n");
+        direcao = opcoesMovimento();
 
-            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
-            da estrutura de repetição*/
-            printf("Digite a quantidade de casas que a sua Torre deve avançar: ");
-            scanf("%d", &movimento);
+        // Solicitando ao usuário o número de movimentações da peça
+        printf("Digite a quantidade de casas que a sua Torre deve avançar: ");
+        scanf("%d", &movimento);
 
-            do
-            {
-                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
-                de movimentação da peça*/
-                printf("Direita\n");
-                contador ++;
-            } while (contador <= movimento);
+        // Passando os paramentros de direção e número de repetições para a função recursiva
+        recursividade(movimento, direcao);
         break;
 
+    // Implementação de Movimentação do Bispo com estrutura WHILE
+    case 2:
+        /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador
+        da estrutura de repetição*/
+        printf("Digite a quantidade de casas que o seu Bispo deve avançar: ");
+        scanf("%d", &movimento);
 
-        // Implementação de Movimentação do Bispo com estrutura WHILE
-        case 2:
-
-            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
-            da estrutura de repetição*/
-            printf("Digite a quantidade de casas que o seu Bispo deve avançar: ");
-            scanf("%d", &movimento);
-
-            while (contador <= movimento)
-            {
-                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
-                de movimentação da peça*/
-                printf("Cima, esquerda\n");
-                contador ++;
-            }
+        while (contador <= movimento)
+        {
+            /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
+            de movimentação da peça*/
+            printf("Cima, esquerda\n");
+            contador++;
+        }
         break;
+
+    // Implementação de Movimentação da Rainha
+    case 3:
+
+        // Solicitando ao usuário e atribuindo a uma string com a direção de movimento da peça
+        printf("\nDigite a direção do movimento da sua Rainha:\n");
+        direcao = opcoesMovimento();
+
+        // Solicitando ao usuário o número de movimentações da peça
+        printf("Digite a quantidade de casas que a sua Rainha deve avançar: ");
+        scanf("%d", &movimento);
+
+        // Passando os paramentros de direção e número de repetições para a função recursiva
+        recursividade(movimento, direcao);
+        break;
+
+    // Implementação de Movimentação do Cavalo
+    case 4:
+        int inicioMovimento, finalMovimento;
+        char *direcaoInicial, *direcaoFinal;
+
+        // Solicitando ao usuário e atribuindo a uma string com a direção de movimento da peça
+        printf("\nDigite a direção do movimento inicial do seu Cavalo:\n");
+        direcaoInicial = opcoesMovimento();
+
+        // Solicitando ao usuário a quantidade de casas para formar o L
+        printf("\nQuantas casas ele deve avançar para %s? 1 ou 2 casas?", direcaoInicial);
+        scanf("%d", &inicioMovimento);
+
+        // Solicitando ao usuário  a direção de movimento final da peça
+        printf("\nDigite a direção final do movimento: \n");
+
+        // Validando se a direção escolhida é vertical ou horizontal e oferencendo as opções opostas para o final do movimento
+        if (direcaoInicial == "Cima" || direcaoInicial == "Baixo")
+        {
+            printf("3 - Para esquerda\n"
+                   "4 - Para direita\n");
+        }
+        else
+        {
+            printf("1 - Para cima\n"
+                   "2 - Para baixo\n");
+        };
+
+        // Atribuindo a variável a direção de movimento final da peça
+        scanf("%d", &opcao);
+
+        // Usando um case para definir a string com a direção escolhida
+        direcaoFinal = validaDirecao(opcao);
+
+        // Definindo a quantidade de casas á percorrer para cada direção
+        finalMovimento = (inicioMovimento == 1) ? 2 : 1;
 
         
-        // Implementação de Movimentação da Rainha com estrutura FOR
-        case 3:
+        // imprime o resultado da opção escolhida pelo usuário no movimento inicial
+        for (int i = 0; i < inicioMovimento; i++)
+        {
+            printf("\n%s ", direcaoInicial);
+        };
 
-            /*Solicitando ao usuário o número de movimentações da peça e atribuindo como contador 
-            da estrutura de repetição*/
-            printf("Digite a quantidade de casas que a sua Rainha deve avançar: ");
-            scanf("%d", &movimento);
+        // imprime o resultado da opção escolhida pelo usuário no movimento final
+        for (int j = 0; j < finalMovimento; j++)
+        {
+            printf("\n%s ", direcaoFinal);
+        };
 
-            for (contador = 1; contador < movimento; contador++)
-            {
-                /*Enquanto a condição for verdadeira o sistema irá imprimir a direção
-                de movimentação da peça*/
-                printf("Esquerda\n");
-            };
         break;
 
-
-
-        // Nível Aventureiro - Movimentação do Cavalo
-        // Implementação de Movimentação do Cavalo com looping aninhado
-        case 4:
-            int movimento =1;
-            int direcaoInicial, direcaoFinal;
-            char *direcao1, *direcao2;
-            //Pedindo ao usuário a direção inicial do movimento
-            printf("\nDigite a direção inicial do movimento do cavalo:\n"
-                "1 - Para cima\n"
-                "2 - Para baixo\n"
-                "3 - Para esquerda\n"
-                "4 - Para direita\n");
-            //Lendo a opção escolhida e armazendo na variável
-            scanf("%d", &direcaoInicial);
-
-
-            //Atribuindo uma string de acordo com o número da opção escolhida para o movimento inicial
-            switch (direcaoInicial)
-            {
-            case 1:
-                direcao1 = "Cima";
-                break;
-            
-            case 2:
-                direcao1 = "Baixo";
-                break;
-            case 3:
-                direcao1 = "Esquerda";
-                break;
-            case 4:
-                direcao1 = "Direita";
-            default:
-                printf("Opção inválida");
-                break;
-            }
-
-
-            //Pedindo ao usuário a direção final do movimento
-            printf("\nDigite a direção final do movimento da sua peça:\n"
-                "1 - Para cima\n"
-                "2 - Para baixo\n"
-                "3 - Para esquerda\n"
-                "4 - Para direita\n");
-            //Lendo a opção escolhida e armazendo na variável
-            scanf("%d", &direcaoFinal);
-
-
-            //Atribuindo uma string de acordo com o número da opção escolhida para o movimento final
-            switch (direcaoFinal)
-            {
-            case 1:
-                direcao2 = "Cima";
-                break;
-            
-            case 2:
-                direcao2 = "Baixo";
-                break;
-            case 3:
-                direcao2 = "Esquerda";
-                break;
-            case 4:
-                direcao2 = "Direita";
-            default:
-                printf("Opção inválida");
-                break;
-            }
-
-
-            //Movimento de looping aninhando para imprimir a movimentação da peça
-            while (movimento--)
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    printf("\n%s",direcao1); //imprime o resultado da opção escolhida pelo usuário no momvimento inicial
-                }
-
-                printf("\n%s", direcao2); //imprime o resultado da opção escolhida pelo usuário no momvimento final
-            };
-        
-        break;
-
-        // Caso nenhum número válido para as opções seja digitado
-        default:
-            printf("Opçãos inválida!");
+    // Caso nenhum número válido para as opções seja digitado
+    default:
+        printf("Opçãos inválida!");
         break;
     };
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
